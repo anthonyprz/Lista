1a8e0e2c58942e1f0e2ebdea379d285fb5df385d
unaiurkiola <unaiurkiola@gmail.com>
2015-09-16 12:14:41 +0200

    insert listo

--- lista/conexion.php
+++ lista/conexion.php
@@ -6,20 +6,16 @@ $pass = $_REQUEST['pass'];
 
 
 $conexion = mysql_connect("localhost","root", "zubiri") or die ("problemas con el servidor");
-
 mysql_select_db("usuarios", $conexion) or die ("error al tratar de conectar");
- 
-$standar =  mysql_query("INSERT INTO id (user, pass) VALUES('$usuario','$pass')");
 
-echo "usuario añadido";
+mysql_query("INSERT INTO id (user, pass) VALUES('$usuario','$pass')");
+
+echo "usuario $usuario añadido";
 
 
 
 //echo "guardado el usuario: $usuario' y contraseña '$pass";
-
 //$standar =  mysql_query("INSERT INTO registro VALUES('','$user','','$password','$favorito','$email','$pais')");
-
-
 //$standar = mysql_query("select * from persona where usuario ='" .$usuario. "'and pass = '" .$pass."'", $conexion);
 //$standar = mysql_query("insert into persona if not exists (usuario, pass) values '".$usuario'","'"");
 //$admin = mysql_query("select * from usuario where login ='" .$usuario. "'and passadmin = '" .$pass."'", $conexion);
